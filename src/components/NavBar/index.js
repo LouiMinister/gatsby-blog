5651805c21bf91cb3a4d7b6a5168d1e6570130b6
LouiMinister <LouiMinister@gmail.com>
2020-03-12 20:26:18 +0900

    코너 깃허브 링크 수정

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -45,7 +45,7 @@ const Navbar = () => (
       >
         <FontAwesomeIcon icon={faBars} />
       </button>
-      <GithubCorner url="https://github.com/calpa/gatsby-starter-calpa-blog" />
+      <GithubCorner url="https://github.com/LouiMinister" />
       <div
         className="collapse navbar-collapse flex-row-reverse"
         id="navbarSupportedContent"
