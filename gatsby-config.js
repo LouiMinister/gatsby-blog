22516d3025a763797b2e6a59a3afb3baa8394a13
LouiMinister <LouiMinister@gmail.com>
2020-04-18 17:16:46 +0900

    Add plugin - gatsby-srouce-notion-database

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,18 @@ module.exports = {
       resolve: 'gatsby-plugin-nprogress',
     },
     {
+      resolve: `gatsby-source-notion-database`,
+      options: {
+        sourceConfig : [
+          {
+            name: 'posts',
+            table: 'https://www.notion.so/conradlin/1aa283fcd5ae4a73ba0f73c062de745e?v=6a40014bee144152b55203e2caf0c02e',
+            cacheType: 'html'
+          }
+        ]
+      }
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: "LouiMinister's Blog",
